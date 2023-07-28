0e1658591349aca19e198ccb23d8ae740f6c990a
Alper Aygördüler <alperaygorduler61@gmail.com>
2023-07-28 11:57:44 +0300

    Delete rickAndMorty/src/components directory

--- rickAndMorty/src/components/home/details/detailCard/detailCard-header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="">
-        <img :src="props.cardImg" alt="" class="w-full h-full">
-    </div>
-</template>
-<script setup>
-import { defineProps } from 'vue';
-const props = defineProps(["cardImg"]);
-</script>
