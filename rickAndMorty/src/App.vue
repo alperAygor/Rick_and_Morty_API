e20d82f09b23bc54f95b14d233c8f5fc175f9ca5
Alper Aygördüler <alperaygorduler61@gmail.com>
2023-07-28 11:59:00 +0300

    Update App.vue

--- rickAndMorty/src/App.vue
+++ rickAndMorty/src/App.vue
@@ -3,38 +3,38 @@
 </template>
 <script setup>
 import { useRouter } from 'vue-router';
-import { useStore } from 'vuex';
+import { useStore } from './store/index';
+import { watchEffect } from 'vue';
 const router = useRouter();
 const store = useStore();
 router.beforeEach((to, from, next) => {
   if (to.path === '/') {
-    store.commit('setInHome', true);
-    store.commit('setInDetails', false);
-    store.commit('setInLocation', false);
-    store.commit('setInEpisode', false);
-  } else if (to.path === '/de // Başka bir sayfaya yönlendirme durumunda tüm değerleri sıfırlatails') {
-    store.commit('setInHome', false);
-    store.commit('setInDetails', true);
-    store.commit('setInLocation', false);
-    store.commit('setInEpisode', false);
+    store.setInHome(true);
+    store.setInDetails(false);
+    store.setInLocation(false);
+    store.setInEpisode(false);
+  } else if (to.path === '/details') {
+    store.setInHome(false);
+    store.setInDetails(true);
+    store.setInLocation(false);
+    store.setInEpisode(false);
   } else if (to.path === '/locations') {
-    store.commit('setInHome', false);
-    store.commit('setInDetails', false);
-    store.commit('setInLocation', true);
-    store.commit('setInEpisode', false);
+    store.setInHome(false);
+    store.setInDetails(false);
+    store.setInLocation(true);
+    store.setInEpisode(false);
   } else if (to.path === '/episodes') {
-    store.commit('setInHome', false);
-    store.commit('setInDetails', false);
-    store.commit('setInLocation', false);
-    store.commit('setInEpisode', true);
+    store.setInHome(false);
+    store.setInDetails(false);
+    store.setInLocation(false);
+    store.setInEpisode(true);
   } else {
-    store.commit('setInHome', false);
-    store.commit('setInDetails', false);
-    store.commit('setInLocation', false);
-    store.commit('setInEpisode', false);
+    store.setInHome(false);
+    store.setInDetails(false);
+    store.setInLocation(false);
+    store.setInEpisode(false);
   }
 
   next();
 });
-
 </script>
